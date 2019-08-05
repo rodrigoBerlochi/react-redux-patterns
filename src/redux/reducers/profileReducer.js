c48d9df4682b93eb9ec2d488f5591eb2138c7c91
Rodrigo Berlochi <rodrigo.berlochi@endava.com>
2019-08-04 23:01:15 -0300

    Review reducer section comments

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -1,10 +1,10 @@
 import defaultState from '../store/defaultState'
 
-// we extract only the slice of the default state we want
+// We extract only the slice of the default state we want
 const { profile } = defaultState
 
-// a reducer function MUST be PURE: operates over its own arguments and return the next state
-// no side effects, no randomness, no ajax
+// A reducer function MUST be PURE: operates over its own arguments and return the next state
+// no side effects, no randomness, no network requests
 export default function profileReducer (state = profile, action) {
     switch (action.type) {
         case 'ACTION_NAME':

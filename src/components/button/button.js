f830137f3b4ad0acc91b41367c62998ed6d68907
Rodrigo Berlochi <rodrigo.berlochi@endava.com>
2019-08-05 10:44:45 -0300

    Added reviews and index for basic presentational components and for React Patterns

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { string, func } from 'prop-types';
 
-export default ({
+// First, outside of this example context, I'd rather have presentational components
+// in their own repository than into the application code. Also I'd include a tool
+// like Storybook in that repo. We wroked with that approach for Tableau, and the experience
+// was excellent. It enforces keeping presentational components truly isolated from specific
+// implementation of the business application. And it means they are reusable and easy to test.
+
+// nothing fancy here for now
+// just a minimal component exported as default
+// that will use in other modules. 
+// It is worth noting that exporting as default a component
+// is considered a good practice and enforced by some linter rules
+// I think it is a good way to allow better semantic on the consumer side
+// sice instead of importing a Button, it can name it in way more meaningful
+// for the usage context, e.g.: PlayButton
+const Button = ({
     text,
     onClick,
 }) => (
@@ -8,3 +23,12 @@ export default ({
         {text}
     </button>
 );
+
+// reducing boilerplate importing directly prop-types members instead of
+// repeating once and again PropType.string and so on. 
+Button.propTypes = {
+    text: string.isRequired,
+    onClick: func.isRequired,
+}
+
+export default Button;
---
+++ src/components/button/button.test.js
@@ -0,0 +1,5 @@
+// TODO: work on tests thoughts here
+
+// I like to have component UT co-located with the component code
+// That way tests are part of the componetization, are easy to find, 
+// harder to forget and less prone to become dead code. 
